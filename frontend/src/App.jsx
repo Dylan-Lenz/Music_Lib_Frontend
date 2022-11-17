b000376e9d24b103e48d5b652c6508fd55dc48dd
unknown <dylanjlenz@gmail.com>
2022-11-17 15:48:21 -0600

    Added CreateSong File + Modified StrictMode + Added Axios to Apps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/music/');
+    console.log(response.data);
+    setSongs(response.data)
+  }
+
   return (
     <div>
+
     </div>
   );
 }
