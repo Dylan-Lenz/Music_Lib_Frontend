ef6442afd15afa12f2cbb66322b9f4cf34b8435b
unknown <dylanjlenz@gmail.com>
2022-11-19 15:19:49 -0600

    Created Initial MusicTable Component

--- frontend/src/Apps.jsx
+++ frontend/src/Apps.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import MusicTable from './components/MusicTable/MusicTable';
+
+
+
 
 function App() {
+  return (
+    <main style={{backgroundImage: `url(https://www.comstocksmag.com/sites/main/files/imagecache/lightbox/main-images/0417_blog_lead_ss.jpg)`}}>
+      <body>
+        <div><MusicTable/></div>
+        <div>SearchBar</div>
+        <div>AddSong</div>
+      </body>
+    </main>
+  );
+}
+export default App;
 
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    getAllSongs();
-  }, []);
-
-
-  async function getAllSongs(){
-    const response = await axios.get('http://127.0.0.1:8000/music/');
-    console.log(response.data);
-    setSongs(response.data)
-  }}
