98d402d492fb2b948c8f6068b2c8c492e65dd510
unknown <dylanjlenz@gmail.com>
2022-11-19 16:08:35 -0600

    Created Initial SearchBar Component

--- frontend/src/Apps.jsx
+++ frontend/src/Apps.jsx
@@ -1,5 +1,6 @@
 import MusicTable from './components/MusicTable/MusicTable';
 import AddSong from './components/CreateSong/CreateSong';
+import SearchBar from './components/SearchBar/SearchBar';
 
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <main style={{backgroundImage: `url(https://www.comstocksmag.com/sites/main/files/imagecache/lightbox/main-images/0417_blog_lead_ss.jpg)`}}>
       <body>
-      <div>SearchBar</div>
+      <div><SearchBar/></div>
         <div>
           <MusicTable/>
           <AddSong/>
---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const SearchBar = (props) => {
+    return ( 
+        <form>
+            <input type='string' />
+            <label>Search Song</label>
+        </form>
+     );
+}
+ 
+export default SearchBar;
